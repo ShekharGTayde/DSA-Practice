46fd02ba8dd4d43c0255806fec0844676944bf2d
Shekhar Tayde <shekhartayde410@gmail.com>
2026-01-28 22:30:51 +0530

    sorting algorithms are practiceds

---
+++ Sorting/basic.cpp
@@ -0,0 +1,153 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// selection sort is all about select min and swap it
+void selection_sort(int arr[], int n)
+{
+    // iterate array
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int min = i;
+        // find min
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+            // swap them
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
+// bubble sort is all about comparing the adjacent element and swap them
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+// insertion sort is all about inserting the element at its correct position
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
+
+// merge sort is all about divide the given arr untill the size become 1 and then merge them in correct order
+void merge(int arr[], int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+void merge_sort(int arr[], int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    merge_sort(arr, low, mid);
+    merge_sort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+// quick sort is all about partitioning the array around a pivot such that left side element are smaller than pivot and right side element are greater than pivot
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+void qs(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    qs(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
